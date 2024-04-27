da2748f0693443111a64bf839dc38bcd03336650
Dark Devil <123370768+darkdevil-dev@users.noreply.github.com>
2024-04-27 22:23:12 +0530

    Update core.py

--- modules/core.py
+++ modules/core.py
@@ -1,52 +1,28 @@
-import os
-import time
-import datetime
-import aiohttp
-import aiofiles
-import asyncio
 import logging
-import requests
-import tgcrypto
 import subprocess
-import concurrent.futures
-
-from utils import progress_bar
-
-from pyrogram import Client, filters
+import datetime
+import asyncio
+import os
+import requests
+import time
+from p_bar import progress_bar
+from config import LOG
+import aiohttp
+import tgcrypto
+import aiofiles
 from pyrogram.types import Message
+from pyrogram import Client, filters
+import concurrent.futures
 
 
 def duration(filename):
-    result = subprocess.run(["ffprobe", "-v", "error", "-show_entries",
-                             "format=duration", "-of",
-                             "default=noprint_wrappers=1:nokey=1", filename],
+    result = subprocess.run([
+        "ffprobe", "-v", "error", "-show_entries", "format=duration", "-of",
+        "default=noprint_wrappers=1:nokey=1", filename
+    ],
                             stdout=subprocess.PIPE,
                             stderr=subprocess.STDOUT)
     return float(result.stdout)
-
-
-def exec(cmd):
-    process = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    output = process.stdout.decode()
-    print(output)
-    return output
-
-
-def pull_run(work, cmds):
-    with concurrent.futures.ThreadPoolExecutor(max_workers=work) as executor:
-        print("Waiting for tasks to complete")
-        fut = executor.map(exec, cmds)
-
-
-async def aio(url, name):
-    k = f'{name}.pdf'
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as resp:
-            if resp.status == 200:
-                f = await aiofiles.open(k, mode='wb')
-                await f.write(await resp.read())
-                await f.close()
-    return k
 
 
 async def download(url, name):
@@ -60,53 +36,10 @@ async def download(url, name):
     return ka
 
 
-def parse_vid_info(info):
-    info = info.strip()
-    info = info.split("\n")
-    new_info = []
-    temp = []
-    for i in info:
-        i = str(i)
-        if "[" not in i and '---' not in i:
-            while "  " in i:
-                i = i.replace("  ", " ")
-            i.strip()
-            i = i.split("|")[0].split(" ", 2)
-            try:
-                if "RESOLUTION" not in i[2] and i[2] not in temp and "audio" not in i[2]:
-                    temp.append(i[2])
-                    new_info.append((i[0], i[2]))
-            except:
-                pass
-    return new_info
-
-
-def vid_info(info):
-    info = info.strip()
-    info = info.split("\n")
-    new_info = dict()
-    temp = []
-    for i in info:
-        i = str(i)
-        if "[" not in i and '---' not in i:
-            while "  " in i:
-                i = i.replace("  ", " ")
-            i.strip()
-            i = i.split("|")[0].split(" ", 3)
-            try:
-                if "RESOLUTION" not in i[2] and i[2] not in temp and "audio" not in i[2]:
-                    temp.append(i[2])
-                    new_info.update({f'{i[2]}': f'{i[0]}'})
-            except:
-                pass
-    return new_info
-
 
 async def run(cmd):
     proc = await asyncio.create_subprocess_shell(
-        cmd,
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE)
+        cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
 
     stdout, stderr = await proc.communicate()
 
@@ -174,21 +107,13 @@ async def download_video(url, cmd, name):
         return os.path.isfile.splitext[0] + "." + "mp4"
 
 
-async def send_doc(bot: Client, m: Message, cc, ka, cc1, prog, count, name):
-    reply = await m.reply_text(f"Uploading » `{name}`")
-    time.sleep(1)
-    start_time = time.time()
-    await m.reply_document(ka, caption=cc1)
-    count += 1
-    await reply.delete(True)
-    time.sleep(1)
-    os.remove(ka)
-    time.sleep(3)
 
+async def send_vid(bot: Client, m: Message, cc, filename, thumb, name):
 
-async def send_vid(bot: Client, m: Message, cc, filename, thumb, name, prog):
-    subprocess.run(f'ffmpeg -i "{filename}" -ss 00:01:00 -vframes 1 "{filename}.jpg"', shell=True)
-    await prog.delete(True)
+    subprocess.run(
+        f'ffmpeg -i "{filename}" -ss 00:01:00 -vframes 1 "{filename}.jpg"',
+        shell=True)
+    await prog.delete (True)
     reply = await m.reply_text(f"**⥣ Uploading ...** » `{name}`")
     try:
         if thumb == "no":
@@ -203,11 +128,16 @@ async def send_vid(bot: Client, m: Message, cc, filename, thumb, name, prog):
     start_time = time.time()
 
     try:
-        await m.reply_video(filename, caption=cc, supports_streaming=True, height=720, width=1280, thumb=thumbnail,
-                             duration=dur, progress=progress_bar, progress_args=(reply, start_time))
+        copy = await bot.send_video(chat_id=m.chat.id,video=filename,caption=cc, supports_streaming=True,height=720,width=1280,thumb=thumbnail,duration=dur) #, progress=progress_bar,progress_args=(reply,start_time))
+    except TimeoutError:
+        await asyncio.sleep(5) 
+        copy = await bot.send_video(chat_id=m.chat.id,video=filename,caption=cc, supports_streaming=True,height=720,width=1280,thumb=thumbnail,duration=dur) #, progress=progress_bar,progress_args=(reply,start_time))
+               
     except Exception:
-        await m.reply_document(filename, caption=cc, progress=progress_bar, progress_args=(reply, start_time))
+        copy = await bot.send_video(chat_id=m.chat.id,video=filename,caption=cc, supports_streaming=True,height=720,width=1280,thumb=thumbnail,duration=dur) #progress=progress_bar,progress_args=(reply,start_time))
+
+
     os.remove(filename)
 
     os.remove(f"{filename}.jpg")
-    await reply.delete(True)
+   # await prog.delete(True)
