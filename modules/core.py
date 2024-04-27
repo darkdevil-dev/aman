b8bdcc87246c39395de3f5445068025fbe9a2cfc
Dark Devil <123370768+darkdevil-dev@users.noreply.github.com>
2024-04-27 22:48:12 +0530

    Update core.py

--- modules/core.py
+++ modules/core.py
@@ -106,8 +106,18 @@ async def download_video(url, cmd, name):
         return os.path.isfile.splitext[0] + "." + "mp4"
 
 
+async def send_doc(bot: Client, m: Message, cc, ka, cc1, prog, count, name):
+    reply = await m.reply_text(f"Uploading » `{name}`")
+    time.sleep(1)
+    start_time = time.time()
+    await m.reply_document(ka,caption=cc1)
+    count+=1
+    await reply.delete (True)
+    time.sleep(1)
+    os.remove(ka)
+    time.sleep(3) 
 
-async def send_vid(bot: Client, m: Message, cc, filename, thumb, name):
+async def send_vid(bot: Client, m: Message,cc, filename, thumb, name, prog):
 
     subprocess.run(
         f'ffmpeg -i "{filename}" -ss 00:01:00 -vframes 1 "{filename}.jpg"',
