b632edcbc03975c15dc8c6672e8092b4257a363c
Dark Devil <123370768+darkdevil-dev@users.noreply.github.com>
2024-04-27 09:41:34 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -7,7 +7,7 @@ from pyrogram import Client
 
 class Config(object):
     # Define your configuration variables here
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7023286569:AAGI_wWdkZMQuF2K5PSfn-4KDX7fcymbG0I")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6645408661:AAFxufz3PRyTbW6SgLVFJYYXmWCP_NFy4_g")
     APP_ID = int(os.environ.get("APP_ID", "24055031"))
     API_HASH = os.environ.get("API_HASH", "8421a60ca38e8dbe23feff45edd91688")
     OWNER_ID = "5631563685"
