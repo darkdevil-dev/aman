ce2e1448cbc6eb2750993e1b2eef9fc14e02ef1c
Dark Devil <123370768+darkdevil-dev@users.noreply.github.com>
2024-04-29 15:01:12 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -24,9 +24,9 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 bot = Client(
     "bot",
-    api_id=api_id,
-    api_hash=api_hash,
-    bot_token=bot_token)
+    api_id=Config.APP_ID,
+    api_hash=Config.API_HASH,
+    bot_token=Config.BOT_TOKEN)
 
 
 @bot.on_message(filters.command(["start"]))
