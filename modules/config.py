2cd67304b144be7594d2c0f0d5d9a5a58fa154d2
Dark Devil <123370768+darkdevil-dev@users.noreply.github.com>
2024-04-29 14:38:00 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -7,8 +7,8 @@ from pyrogram import Client
 
 class Config(object):
     # Define your configuration variables here
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7023286569:AAGI_wWdkZMQuF2K5PSfn-4KDX7fcymbG0I")
-    APP_ID = int(os.environ.get("APP_ID", "26945844"))
-    API_HASH = os.environ.get("API_HASH", "9f2f1d81fd2af2b21a700fa6681215b1")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7150060423:AAHg8r5e39v0EB-rKIzwHeQbwrYXamc-uaI")
+    APP_ID = int(os.environ.get("APP_ID", "24055031"))
+    API_HASH = os.environ.get("API_HASH", "8421a60ca38e8dbe23feff45edd91688")
     OWNER_ID = "5631563685"
     MONGO_URI = "mongodb+srv://alonebeats:alonebeats@cluster0.vydighs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
