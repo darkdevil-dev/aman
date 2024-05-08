a9db31a7f0d97b8f61e1ba1f9d875e9a4b1c3134
Dark Devil <123370768+darkdevil-dev@users.noreply.github.com>
2024-05-08 20:52:12 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -8,7 +8,7 @@ from pyrogram import Client
 class Config(object):
     # Define your configuration variables here
     BOT_TOKEN = os.environ.get('BOT_TOKEN')
-    APP_ID = int(os.environ.get('APP_ID'))
+    API_ID = int(os.environ.get('APP_ID'))
     API_HASH = os.environ.get('API_HASH')
     OWNER_ID = int(os.environ.get('OWNER_ID'))
     MONGO_URI = os.environ.get('MONGO_URI')
