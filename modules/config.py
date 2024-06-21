c55d4b2d9491ff7cc8f23a4f1cf767c46f808d3b
Dark Devil <123370768+darkdevil-dev@users.noreply.github.com>
2024-06-21 06:17:00 +0530

    Create config.py

---
+++ modules/config.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) ACE 
+
+import os
+from pyrogram import Client
+
+class Config(object):
+    # Define your configuration variables here
+    BOT_TOKEN = '7295982155:AAEPBhvrhe3lqRPDlE1733nB7bBX2bzOfFA'
+    API_ID = 26945844
+    API_HASH = '9f2f1d81fd2af2b21a700fa6681215b1'
+    OWNER_ID = 5631563685
+    MONGO_URI = 'mongodb+srv://alonebeats:alonebeats@cluster0.vydighs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+    CHANNEL_ID = -1002020948245
+
+    
+    
+  
